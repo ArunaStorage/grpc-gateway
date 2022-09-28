5d45b682b93d976b3e4907c0a57c3f36481356c7
St4NNi <sb@pus.de>
2022-09-28 13:02:26 +0200

    Added debug statement to print full config

--- config/config.go
+++ config/config.go
@@ -20,9 +20,12 @@ const (
 func HandleConfigFile() {
 	viper.SetConfigName("config") // name of config file (without extension)
 	viper.SetConfigType("yaml")   // REQUIRED if the config file does not have the extension in the name
+	viper.AddConfigPath("/config")
 	viper.AddConfigPath("./config")
 	err := viper.ReadInConfig() // Find and read the config file
 	if err != nil {             // Handle errors reading the config file
 		panic(fmt.Errorf("fatal error config file: %w", err))
 	}
+
+	fmt.Println(viper.AllSettings())
 }
